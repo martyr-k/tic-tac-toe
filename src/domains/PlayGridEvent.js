8724dd6b7144bb6a10797c9a8c3da0015b8f46d6
martyrk <admin@martyrk.design>
2022-09-18 19:10:33 -0400

    delete old files

--- src/domains/PlayGridEvent.js
+++
@@ -1,6 +0,0 @@
-export const isGridPlayEvent = (e) => e.event === "gridPlay";
-
-export const isNoWinnerEvent = (e) => e.event === "gridNoWinner";
-
-export const isGameOverEvent = (e) =>
-  e.event === "gridWinner" || e.event === "gridFilled";
