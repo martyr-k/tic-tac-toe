fd9c5c2578cafd3fff69085ecb35011fab8b1fb4
martyrk <admin@martyrk.design>
2022-10-09 09:09:13 -0400

    add logic for handling multiplayer

--- server/index.js
+++ server/index.js
@@ -8,19 +8,56 @@ const io = new Server(server, {
   cors: {},
 });
 
-const connections = [];
+const players = [];
+let activePlayer;
 
 io.on("connection", (socket) => {
-  console.log("A user connected");
-  connections.push(socket);
-  console.log(connections.length);
+  // player join
+  socket.on("player join", (data) => {
+    // prevent more than two players from joining
+    if (players.length < 2) {
+      socket.player = data;
+      players.push(socket.player);
+      console.log(`A player, ${data} joined.`);
+
+      const playerPos = getPlayerPos(players, socket.player);
+
+      socket.emit("set player", playerPos);
+
+      if (players.length === 1) {
+        activePlayer = playerPos;
+      }
+
+      io.sockets.emit("set active player", activePlayer);
+    }
+  });
+
+  // player move
+  socket.on("play", ({ index, activePlayer }) => {
+    // send play to all connected clients
+    io.sockets.emit("add play", { index, activePlayer });
+
+    // send next player to all clients
+    const nextPlayer = activePlayer === 1 ? 2 : 1;
+    io.sockets.emit("set active player", nextPlayer);
+  });
+
+  // start new game
+  socket.on("new game", () => {
+    io.sockets.emit("start new game");
+    io.sockets.emit("set active player", 1);
+  });
 
   socket.on("disconnect", () => {
-    connections.splice(connections.indexOf(socket), 1);
-    console.log("A user disconnected");
+    players.splice(players.indexOf(socket), 1);
+    console.log("A player left.");
   });
 });
 
 server.listen(3001, () => {
   console.log("Server listening on port 3001...");
 });
+
+function getPlayerPos(players, player) {
+  return players.findIndex((p) => p === player) + 1;
+}
